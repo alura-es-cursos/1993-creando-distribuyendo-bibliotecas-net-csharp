ff6b1faa655d535fb5396f2e3d56833b70629667
Leonardo Jose <ljcl79@gmail.com>
2023-03-25 10:09:46 -0300

    Aula4

---
+++ AppLlaves/AppLlaves/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ByteBank.GeneradorLlaves;
+
+Console.WriteLine("Aplicación de llaves");
+
+Console.WriteLine(GeneradorLlaves.GeneraLlaveUnica());
+var llaves = GeneradorLlaves.GeneraLlavesUnicas(5);
+
+for(int i=0;i < llaves.Count();i++)
+{
+    Console.WriteLine(llaves[i]);
+}
+Console.ReadLine();
